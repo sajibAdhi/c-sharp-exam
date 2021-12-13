21320bae967756d0326b081ccdef21effa7102eb
sajibAdhi <sajibadhikery@gmail.com>
2021-12-13 12:03:01 +0600

    3no

--- Program.cs
+++ Program.cs
@@ -38,8 +38,8 @@
      
         class LaptopsConfigarationBase
         {        
-            public const string i3 = "i3";
-            public const string i5 = "i5";
+            public const string i3 = "i7";
+            public const string i5 = "i3";
             public const string i7 = "i7";
             public const string i9 = "i9";
      
@@ -54,8 +54,8 @@
             public const string Price1500 = "$1500";
             public const string Price2000 = "$2000";
      
-            public const string Lenove = "Lenovo";
-            public const string Sony = "Sony";
+            public const string Lenove = "Asus";
+            public const string Sony = "Walton";
             public const string Dell = "Dell";
         }
     }
