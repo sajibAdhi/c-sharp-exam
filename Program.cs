4f9d42ebddd052f275df4962a851b87133936861
sajibAdhi <sajibadhikery@gmail.com>
2021-12-13 11:45:14 +0600

    1no

--- Program.cs
+++ Program.cs
@@ -6,16 +6,29 @@ using System.Threading.Tasks;
 
 namespace Exercise
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
+            System.Collections.Generic.List<Shape> shapes = new System.Collections.Generic.List<Shape>();
+            shapes.Add(new Rectangle());
+            shapes.Add(new Triangle());
+            shapes.Add(new Circle());
+
+            foreach (Shape s in shapes)
+            {
+                s.Draw();
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+    }
 
     public class Shape
     {
-        // A few example members
-        public int X { get; private set; }
-        public int Y { get; private set; }
-        public int Height { get; set; }
-        public int Width { get; set; }
-
         // Virtual method
         public virtual void Draw()
         {
@@ -27,7 +40,6 @@ namespace Exercise
     {
         public override void Draw()
         {
-            // Code to draw a circle...
             Console.WriteLine("Drawing a circle");
             base.Draw();
         }
@@ -36,7 +48,6 @@ namespace Exercise
     {
         public override void Draw()
         {
-            // Code to draw a rectangle...
             Console.WriteLine("Drawing a rectangle");
             base.Draw();
         }
@@ -45,35 +56,8 @@ namespace Exercise
     {
         public override void Draw()
         {
-            // Code to draw a triangle...
             Console.WriteLine("Drawing a triangle");
             base.Draw();
         }
     }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
-            System.Collections.Generic.List<Shape> shapes = new System.Collections.Generic.List<Shape>();
-            shapes.Add(new Rectangle());
-            shapes.Add(new Triangle());
-            shapes.Add(new Circle());
-
-            // Polymorphism at work #2: the virtual method Draw is
-            // invoked on each of the derived classes, not the base class.
-            foreach (Shape s in shapes)
-            {
-                s.Draw();
-            }
-
-            // Keep the console open in debug mode.
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
-        }
-
-    }
-
-
 }
